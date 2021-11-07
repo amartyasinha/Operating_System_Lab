2dfb646fedbe98a83bfd8e5c74707053a767067a
amartya <amartyasinha918@gmail.com>
2021-11-07 20:11:13 +0530

    New Commit OS

--- LabWork12_ThreadFactorial.c
+++ LabWork12_ThreadFactorial.c
@@ -5,12 +5,13 @@
 #include<stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
-int factorial; /* this data is shared by the thread(s) */
+
+int factorial; // this data is shared by the thread(s)
 void *runner(void *param);
 int main(int argc, char *argv[])    //5 is set in configuration
 {
-    pthread_t tid; /* the thread identifier */
-    pthread_attr_t attr; /* set of thread attributes */
+    pthread_t tid; // the thread identifier
+    pthread_attr_t attr; // set of thread attributes
     if (argc != 2) {
         fprintf(stderr,"usage: a.out <integer value>\n");
         return -1;
@@ -19,15 +20,15 @@ int main(int argc, char *argv[])    //5 is set in configuration
         fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
         return -1;
     }
-/* get the default attributes */
+// get the default attributes
     pthread_attr_init(&attr);
-/* create the thread */
+// create the thread
     pthread_create(&tid,&attr,runner,argv[1]);
-/* wait for the thread to exit */
+// wait for the thread to exit
     pthread_join(tid,NULL);
     printf("factorial = %d\n", factorial);
 }
-/* The thread will begin control in this function */
+// The thread will begin control in this function
 void *runner(void *param)
 {
     int i, upper = atoi(param);
--- LabWork1_fork_exec.c
+++ LabWork1_fork_exec.c
@@ -8,24 +8,36 @@
 #include <sys/wait.h>
 
 int main() {
-    pid_t pid;
-    /* fork a child process */
-    pid = fork();
-    printf("processID = %d\n", pid);
+    pid_t processID, p;
+    printf("------------------------\n");
+    printf("Same Program, Same Code\n");
+    printf("------------------------\n");
+    // fork a child process
+    p = fork();
+    processID = getpid();
 
-    if (pid < 0) {
-        /* error occurred */
+    printf("Output of fork: %d\n", p);
+    printf("Process ID: %d\n", processID);
+
+    if (p < 0) {
+        // error occurred
         fprintf(stderr, "Fork Failed");
         return 1;
     }
-    else if (pid == 0) {
-        /* child process */
+    else if (p == 0) {
+        // child process
+        printf("-----------------------------\n");
+        printf("Same Program, Different Code\n");
+        printf("-----------------------------\n");
         execlp("/bin/ls","ls",NULL);
     }
     else {
-        /* parent process */
-        /* parent will wait for the child to complete */
+        // parent process
+        // parent will wait for the child to complete
         wait(NULL);
+        printf("----------------------------------------------------------------------\n");
+        printf("Before Terminating, the Parent waits for the child to finish its task\n");
+        printf("----------------------------------------------------------------------\n");
         printf("Child Complete");
     }
 
