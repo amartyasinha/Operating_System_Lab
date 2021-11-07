abfd3d3a00d30f0438c1a6cb0f317f520dd156cf
amartya <amartyasinha918@gmail.com>
2021-11-07 22:16:01 +0530

    New Commit OS

--- LabWork12_Threads.c
+++ LabWork12_Threads.c
@@ -7,46 +7,46 @@
 #include<stdlib.h>
 
 int sum, factorial;
-void *runnerSum(void *param);   // thread will call this function for sum
-void *runnerFact(void *param);  // thread will call this function for factorial
 
-int main(int argc , char *argv[])  //5 is set in configuration
-{
-    pthread_t tid;
-    pthread_attr_t attr;
-    if(argc!=2)
-    {
-        fprintf(stderr,"Usage : a.out<integer value>\n");
-        return -1;
-    }
-    if(atoi(argv[1])<0)
-    {
-        fprintf(stderr,"%d must be >=0\n",atoi(argv[1]));
-        return -1;
-    }
-    pthread_attr_init(&attr);
-    pthread_create(&tid, &attr, runnerSum, argv[1]);
-    pthread_create(&tid, &attr, runnerFact, argv[1]);
-    pthread_join(tid,NULL);
-    printf("Sum = %d\n",sum);
-    printf("Factorial = %d\n",factorial);
-}
-
-void *runnerSum(void *param)
-{
-    int i , upper = atoi(param);
+// thread will call this function for sum
+void *runnerSum(void *param) {
+    int i, upper = atoi(param);
     sum = 0;
-    for(i = 1; i <= upper; i++)
-    {
+    for (i = 1; i <= upper; i++) {
         sum += i;
     }
     pthread_exit(0);
 }
 
-void *runnerFact(void *param){
+// thread will call this function for factorial
+void *runnerFact(void *param) {
     int i, upper = atoi(param);
     factorial = 1;
     for (i = 1; i <= upper; i++)
         factorial *= i;
     pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {  //5 is set in configuration
+
+    pthread_t tidSum, tidFact;
+    pthread_attr_t attrSum, attrFact;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./a.out <integer value>\n");
+        return -1;
+    }
+    if (atoi(argv[1]) < 0) {
+        fprintf(stderr, "%d must be >= 0\n", atoi(argv[1]));
+        return -1;
+    }
+
+    pthread_attr_init(&attrSum);
+    pthread_create(&tidSum, &attrSum, runnerSum, argv[1]);
+    pthread_join(tidSum, NULL);
+    printf("Sum of first %d numbers is %d\n", atoi(argv[1]), sum);
+
+    pthread_attr_init(&attrFact);
+    pthread_create(&tidFact, &attrFact, runnerFact, argv[1]);
+    pthread_join(tidFact, NULL);
+    printf("Factorial of first %d numbers is %d\n", atoi(argv[1]), factorial);
 }
